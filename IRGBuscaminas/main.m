9cc2774bfee11c47b490f61deb64a32d2d0dce56
Ivan Rodriguez <ivan.rodriguez.gonzalez@gmail.com>
2014-11-06 16:30:19 +0100

    Initial Commit

---
+++ IRGBuscaminas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IRGBuscaminas
+//
+//  Created by Leticia Vila Sexto on 6/11/14.
+//  Copyright (c) 2014 Ivan Rodriguez Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
